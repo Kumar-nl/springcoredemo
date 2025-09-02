ed76398e43fe08d3065cd2b85514ea28e6985ea5
Kumar Sagar <sagar@lifeguru.app>
2025-09-02 19:28:40 +0530

    Java config bean to inject third party using bean annotation

--- src/main/java/com/buildwithkumar/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/buildwithkumar/springcoredemo/common/CricketCoach.java
@@ -1,9 +1,9 @@
 package com.buildwithkumar.springcoredemo.common;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
+//import jakarta.annotation.PostConstruct;
+//import jakarta.annotation.PreDestroy;
+//import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+//import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,16 +14,16 @@ public class CricketCoach implements Coach {
     }
 
     //define your init method
-    @PostConstruct
-    public void doMyStartupStuff() {
-        System.out.println("In doMyStartupStuff: " + getClass().getSimpleName());
-    }
+//    @PostConstruct
+//    public void doMyStartupStuff() {
+//        System.out.println("In doMyStartupStuff: " + getClass().getSimpleName());
+//    }
 
     //define our destroy method
-    @PreDestroy
-    public void doMyCleanupStuff() {
-        System.out.println("In doMyCleanupStuff: " + getClass().getSimpleName());
-    }
+//    @PreDestroy
+//    public void doMyCleanupStuff() {
+//        System.out.println("In doMyCleanupStuff: " + getClass().getSimpleName());
+//    }
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15 minutes :)";
